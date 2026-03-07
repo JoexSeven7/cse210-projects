dc1576a54b67d73d5f86ad14b588b7b5563d603c
JoexSeven7 <johnjoshuaaaaaa@gmail.com>
2026-03-07 14:25:54 +0100

    add grade evaluation logic to Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Enter Your Grade Number: ");
+        string gradeNumber = Console.ReadLine();
+
+        int grade = int.Parse(gradeNumber);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your Grade is A");
+        }
+
+        else if (grade >= 85)
+        {
+            Console.WriteLine("Your Grade is A-");
+        }
+
+         else if (grade >= 82)
+        {
+            Console.WriteLine("Your Grade is B+");
+        }
+        
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your Grade is B");
+        }
+
+       
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your Grade is C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your Grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your Grade is F");
+        }
     }
 }
