5b5439db5032b6d57775f5b0f93f22d56580456f
JoexSeven7 <johnjoshuaaaaaa@gmail.com>
2026-03-08 08:35:36 +0100

    add magic number guessing game in Exercise3 and enhance number processing in Exercise4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,52 @@
 using System;
 
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int>numbers = new List<int>();
+
+        while (true)
+        {
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number != 0)
+        {
+            numbers.Add(number);
+        }
+
+        int sum = numbers.Sum();
+  
+
+        double average = (double)sum / numbers.Count;
+
+        int maximum = numbers.Max();
+
+        List<int> positiveNumbers = numbers.Where(n => n > 0).ToList();
+        int smallestPositive = positiveNumbers.Min();
+        numbers.Sort();
+              
+        Console.WriteLine($"The sum is: {sum}");
+
+        Console.WriteLine($"The average is: {average}");
+
+        Console.WriteLine($"The largest number is: {maximum}");
+        
+        Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+
+        Console.WriteLine("The sorted list is:");
+        
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+        
+    
     }
-}
+
+}}
