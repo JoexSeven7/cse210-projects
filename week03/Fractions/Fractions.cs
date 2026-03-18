83b6eb704f3124e3b2638a73c7120f37a3a27f45
JoexSeven7 <johnjoshuaaaaaa@gmail.com>
2026-03-18 15:32:31 +0100

    add Fractions class to manage fraction operations and calculations

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,43 @@
+public class Fractions
+{
+    private int _top;
+    private int _bottom;
+
+
+    public Fractions ()
+    {
+       _top = 1;
+       _bottom = 1;
+
+    }
+
+    public Fractions (int top)
+    {
+        _top = top;
+         _bottom = 1;
+    }
+
+    public Fractions (int top, int bottom)
+    {
+        _top = top;
+         _bottom = bottom;
+    }
+
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+
+
+
+     public double GetDecimalValue()
+    {
+        double number =  (double)_top / (double)_bottom;
+        return number;
+       
+    }
+
+        
+}
