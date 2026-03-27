29b65164132a18982f4d491f864f3309a914590e
JoexSeven7 <johnjoshuaaaaaa@gmail.com>
2026-03-27 01:13:00 +0100

    added Sceenshot of execution of my project

--- week04/YouTubeVideos/Video.cs
+++ week04/YouTubeVideos/Video.cs
@@ -4,9 +4,37 @@ public class Video
    public string _title;
    public string _author;
    public int _length;
-   public List<Comment>  _comments;
+   public List<Comment> _comments;
 
 
-
+   public Video (string title, string author, int length)
+    {
+      _title = title;
+      _author = author;
+      _length = length;
+      _comments = new List<Comment>(); 
+    }
    
+
+   public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetNumberofComments()
+    {
+        return _comments.Count;
+    }
+
+     public string GetTitle()
+    {
+        return _title;
+    }
+    public string GetAuthor() {
+        return _author;
+        }
+    public int GetLength()
+    {
+        return _length;
+    }
 }
